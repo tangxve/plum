8750337fc1691c6d01f914ef0448774fa11b9bcf
tangxve <11183504@qq.com>
2022-03-30 19:31:35 +0800

    feat: 开发 一个树枝

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@ const ctx = $computed(() => el!.getContext('2d')!)
 const WIDTH = 300
 const HEIGHT = 300
 
-interface Point{
+interface Point {
   x: number
   y: number
 }
 
-interface Line {
+interface Branch {
   start: Point
   length: number
   // 角度
@@ -25,10 +25,34 @@ interface Line {
 
 function init() {
   ctx.strokeStyle = '#fff'
-  const startPoint = { x: WIDTH / 2, y: HEIGHT }
-  const endPoint = { x: WIDTH / 2, y: HEIGHT / 2 }
-  line(startPoint, endPoint)
-  line(endPoint, { x: WIDTH / 2 + 50, y: 10 })
+
+  const branch = {
+    start: {
+      x: WIDTH / 2,
+      y: HEIGHT,
+    },
+    length: 100,
+    theta: -Math.PI / 2,
+  }
+
+  const end = getEndPoint(branch)
+  drawBranch(branch)
+
+  const leftBranch = {
+    start: end,
+    length: 100,
+    theta: branch.theta - 0.1,
+  }
+
+  drawBranch(leftBranch)
+
+  const rightBranch = {
+    start: end,
+    length: 100,
+    theta: branch.theta + 0.1,
+  }
+
+  drawBranch(rightBranch)
 }
 
 function lineTo(p1: Point, p2: Point) {
@@ -38,13 +62,25 @@ function lineTo(p1: Point, p2: Point) {
   ctx.stroke()
 }
 
-function line(l: Line) {
-  const { start, length, theta } = l
-  const end = {
-    x: start.x + length * Math.cos(theta),
+function getEndPoint(b: Branch) {
+  const {
+    start,
+    length,
+    theta,
+  } = b
+  return {
+    x: start.x + length * Math.cos(theta), // 三角函数计算
     y: start.y + length * Math.sin(theta),
   }
-  lineTo(start, end)
+}
+
+function drawBranch(b: Branch) {
+  const {
+    start,
+    length,
+    theta,
+  } = b
+  lineTo(start, getEndPoint(b))
 }
 
 onMounted(() => {
