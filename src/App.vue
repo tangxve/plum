4487089ee7c32cbf89aac16a98ba53d679844611
唐雪 <tangxue@pingpongx.com>
2022-03-31 20:18:01 +0800

    feat: 完整

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@ import { $computed } from 'vue/macros'
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el!.getContext('2d')!)
 
+// console.log('ctx', ctx)
+
 const WIDTH = 600
 const HEIGHT = 600
 
@@ -29,34 +31,59 @@ function init() {
   // 开始的第一个支
   setp({
     start: { x: WIDTH / 2, y: HEIGHT },
-    length: 30,
+    length: 20,
     theta: -Math.PI / 2,
   })
 }
 
-function setp(b: Branch) {
+const pendingTasks: Function[] = []
+
+function setp(b: Branch, depth = 0) {
   const end = getEndPoint(b)
   drawBranch(b)
 
   // 50% 几率长左边支
-  if (Math.random() < 0.5) {
+  if (depth < 5 || Math.random() < 0.5) {
+    // 先缓存起来，优先广度生长
+    pendingTasks.push(() => {
     // leftBranch
-    setp({
-      start: end,
-      length: b.length,
-      theta: b.theta - 0.2,
+      return setp({
+        start: end,
+        length: b.length + (Math.random() * 10 - 5),
+        theta: b.theta - 0.3 * Math.random(),
+      }, depth + 1)
     })
   }
   // 50% 几率长右边支
-  if (Math.random() < 0.5) {
-    // rightBranch
-    setp({
-      start: end,
-      length: b.length,
-      theta: b.theta + 0.2,
+  if (depth < 5 || Math.random() < 0.5) {
+    // 先缓存起来，优先广度生长
+    pendingTasks.push(() => {
+      // rightBranch
+      return setp({
+        start: end,
+        length: b.length + (Math.random() * 10 - 5),
+        theta: b.theta + 0.3 * Math.random(),
+      }, depth + 1)
     })
   }
 }
+function frame() {
+  const tasks = [...pendingTasks]
+  pendingTasks.length = 0
+  tasks.forEach(fn => fn())
+}
+
+let frameCount = 0
+function startFrame() {
+  requestAnimationFrame(() => {
+    frameCount += 1
+    if (frameCount % 3 === 0)
+      frame()
+    startFrame()
+  })
+}
+
+startFrame()
 
 function lineTo(p1: Point, p2: Point) {
   ctx.beginPath()
