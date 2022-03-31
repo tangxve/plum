564b41e15c6dda4fd6c9b2824a5e903b05152199
tangxve <11183504@qq.com>
2022-03-31 19:41:09 +0800

    feat: 完成深度生长

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="el" width="300" height="300" border />
+  <canvas ref="el" width="600" height="600" border />
 </template>
 
 <script setup lang="ts">
@@ -8,8 +8,8 @@ import { $computed } from 'vue/macros'
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el!.getContext('2d')!)
 
-const WIDTH = 300
-const HEIGHT = 300
+const WIDTH = 600
+const HEIGHT = 600
 
 interface Point {
   x: number
@@ -26,33 +26,36 @@ interface Branch {
 function init() {
   ctx.strokeStyle = '#fff'
 
-  const branch = {
-    start: {
-      x: WIDTH / 2,
-      y: HEIGHT,
-    },
-    length: 100,
+  // 开始的第一个支
+  setp({
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 30,
     theta: -Math.PI / 2,
+  })
+}
+
+function setp(b: Branch) {
+  const end = getEndPoint(b)
+  drawBranch(b)
+
+  // 50% 几率长左边支
+  if (Math.random() < 0.5) {
+    // leftBranch
+    setp({
+      start: end,
+      length: b.length,
+      theta: b.theta - 0.2,
+    })
   }
-
-  const end = getEndPoint(branch)
-  drawBranch(branch)
-
-  const leftBranch = {
-    start: end,
-    length: 100,
-    theta: branch.theta - 0.1,
+  // 50% 几率长右边支
+  if (Math.random() < 0.5) {
+    // rightBranch
+    setp({
+      start: end,
+      length: b.length,
+      theta: b.theta + 0.2,
+    })
   }
-
-  drawBranch(leftBranch)
-
-  const rightBranch = {
-    start: end,
-    length: 100,
-    theta: branch.theta + 0.1,
-  }
-
-  drawBranch(rightBranch)
 }
 
 function lineTo(p1: Point, p2: Point) {
